6e5662f4d0c05b46ea5f21e4c54cc0855eaa8d2b
RobiDomin <104741060+RobiDomin@users.noreply.github.com>
2024-11-24 12:05:18 +0100

    Zmian a kodu

---
+++ Obstakel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Obstakel
+
+    {
+
+        public int Xpos { get; set; }
+
+        // ?
+
+        public ConsoleColor schermKleur { get; set; }
+
+        public string karacter { get; set; }
+
+    }
+}
